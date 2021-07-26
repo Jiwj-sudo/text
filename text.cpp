c6fcc20777cd82d39e2b4156fcff6d5adcaa7f89
纪伟军 <80587748+Jiwj-sudo@users.noreply.github.com>
2021-07-26 20:00:29 +0800

    Add files via upload

--- text.cpp
+++ text.cpp
@@ -3,17 +3,35 @@
 
 using namespace std;
 
+struct date
+{
+	int year;
+	int month;
+	int day;
+};
+
+typedef struct Student
+{
+	char id[13];				//学号
+	char name[9];				//姓名
+	struct date birthday;		//生日	
+	char sex[3];					//性别
+	char sfzh[19];				//身份证号
+	char address[61];			//家庭住址
+	char telNumber[12];			//电话号码
+	char className[21];			//班级名称
+}Stu;
+
 int main()
 {
-	int n = 9;
-	float* pFloat = (float*)&n;
-	printf("n的值为：%d\n", n);
-	printf("*pFloat的值为：%f\n", *pFloat);
-	*pFloat = 9.0;
-	printf("n的值为:%d\n", n);
-	printf("*pFloat的值为: %f\n", *pFloat);
-	/*int m;
-	while (cin >> m)
-		printf("%d\n", m);*/
+	Stu s1 = { "202026203002", "纪伟军", {20001, 2, 18}, "男", "362322200002186617", 
+				"南昌市东湖区", "18470137121", "数据科学与大数据技术" };
+	cout << "学    号:" << s1.id << endl;
+	cout << "姓    名:" << s1.name << endl;
+	cout << "出生日期:" << s1.birthday.year << "-" << s1.birthday.month << "-" << s1.birthday.day << endl;
+	cout << "性    别:" << s1.sex << endl;
+	cout << "身份证号:" << s1.sfzh << endl;
+	cout << "电话号码:" << s1.telNumber << endl;
+	cout << "家庭住址:" << s1.address << endl;
 	return 0;
 }
